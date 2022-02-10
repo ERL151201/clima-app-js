5ebc065f459c66d9107352d67aa0d4923c195e74
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-02-10 10:50:24 -0600

    creando la manipulacion del Dom

---
+++ src/app/UI.js
@@ -0,0 +1,11 @@
+class UI {
+
+    constructor() {
+        this.location = document.getElementById('weather-location');
+        this.desc = document.getElementById('weather-description');
+        this.string = document.getElementById('weather-string');
+        this.humidity = document.getElementById('weather-humidity');
+        this.wind = document.getElementById('weather-wind');
+    }
+
+}
---
+++ src/app/weather.js
@@ -0,0 +1,15 @@
+export class Weather {
+
+    constructor(city, countryCode) {
+        this.apikey = '41baf8c48d7c2809016900cd5353bf33';
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+
+    async getWeather() {
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const response = await fetch(URI);
+        const data = await response.json();
+        return data;
+    }
+}
