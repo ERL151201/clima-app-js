2a4f243b2618c7d6320cc85e29730ddfd8e002d0
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-02-10 13:05:02 -0600

    proyecto terminado

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,12 @@
 require('./index.css');
 const { Weather } = require('./Weather');
 const { UI } = require('./UI');
+const { Store } = require('./store');
 
+const store = new Store();
+const { city, countryCode } = store.getLocationData();
 const ui = new UI();
-const weather = new Weather('London', 'uk');
+const weather = new Weather(city, countryCode);
 
 
 
@@ -17,6 +20,7 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const city = document.getElementById('city').value;
     const countryCode = document.getElementById('countryCode').value;
     weather.changeLocation(city, countryCode);
+    store.setLocationData(city, countryCode);
     fetchWeather();
     e.preventDefault();
 });
