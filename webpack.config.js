fcf514f0d91f00f1196f708336db989e1e3a4196
ERL151201 <64818042+ERL151201@users.noreply.github.com>
2022-02-10 14:12:09 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/app/index.js',
     output: {
-        path: path.join(__dirname, 'index.html'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     devServer: {
