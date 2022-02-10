e2ad418260cd4d08da3688ad89b13d5110c8e55d
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-02-10 11:50:09 -0600

    mostrando datos de otras ciudades

--- src/app/UI.js
+++ src/app/UI.js
@@ -1,4 +1,4 @@
-class UI {
+export class UI {
 
     constructor() {
         this.location = document.getElementById('weather-location');
@@ -6,6 +6,14 @@ class UI {
         this.string = document.getElementById('weather-string');
         this.humidity = document.getElementById('weather-humidity');
         this.wind = document.getElementById('weather-wind');
+    }
+
+    render(weather) {
+        this.location.textContent = weather.name + ' / ' + weather.sys.country;
+        this.desc.textContent = weather.weather[0]['description'];
+        this.string.textContent = weather.main.temp + '°C';
+        this.humidity.textContent = 'Humedad: ' + weather.main.humidity + ' %';
+        this.wind.textContent = 'Viento: ' + weather.wind.speed + ' m/s ';
     }
 
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,24 @@
 require('./index.css');
 const { Weather } = require('./Weather');
+const { UI } = require('./UI');
+
+const ui = new UI();
 const weather = new Weather('London', 'uk');
+
+
 
 async function fetchWeather() {
     const data = await weather.getWeather();
     console.log(data);
+    ui.render(data);
 }
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const countryCode = document.getElementById('countryCode').value;
+    weather.changeLocation(city, countryCode);
+    fetchWeather();
+    e.preventDefault();
+});
 
 document.addEventListener('DOMContentLoaded', fetchWeather);
